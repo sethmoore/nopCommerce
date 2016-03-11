05c4dc54b3b64dfe182e4f2a7a33a9265646b1ef
skoshelev <sergey.k@nopcommerce.com>
2016-03-11 14:46:16 +0300

    #210 Move back to browscap.org to detect search crawlers

---
+++ src/Libraries/Nop.Services/Helpers/BrowscapXmlParser.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+using Nop.Core;
+
+namespace Nop.Services.Helpers
+{
+    /// <summary>
+    /// Helper class for working with XML file of Browser Capabilities Project (http://browscap.org/)
+    /// </summary>
+    public class BrowscapXmlHelper
+    {
+        private readonly List<string> _crawlerUserAgents;
+
+        public BrowscapXmlHelper(string filePath)
+        {
+            _crawlerUserAgents = new List<string>();
+
+            if (!File.Exists(filePath))
+                return;
+
+            var sr = new StreamReader(filePath);
+            var text = sr.ReadToEnd().Replace("&", "&amp;");
+
+            var browsercapItems = XDocument.Parse(text).Root.Return(x => x.Element("browsercapitems"), null);
+
+            if (browsercapItems == null)
+                throw new Exception("Incorrect file format");
+
+            _crawlerUserAgents.AddRange(browsercapItems.Elements("browscapitem")
+                //only crawlers
+                .Where(IsBrowscapItemIsCrawler)
+                //get only user agent names
+                .Select(e => e.Attribute("name").Return(a => a.Value.Replace("&amp;", "&"), ""))
+                .Where(s => !string.IsNullOrEmpty(s)));
+        }
+
+        private static bool IsBrowscapItemIsCrawler(XElement browscapItem)
+        {
+            var el = browscapItem.Elements("item").FirstOrDefault(e => e.Attribute("name").Return(a => a.Value, "") == "Crawler");
+
+            return el == null ? false : el.Attribute("value").Return(a => a.Value.ToLower() == "true", false);
+        }
+
+        /// <summary>
+        /// Determines whether a user agent is a crawler
+        /// </summary>
+        /// <param name="userAgent">User agent string</param>
+        /// <returns>True if user agent is a crawler, otherwise - false</returns>
+        public bool IsCrawler(string userAgent)
+        {
+            return _crawlerUserAgents.Any(p => p == userAgent);
+        }
+    }
+}
